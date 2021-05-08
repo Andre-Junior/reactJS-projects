9f1557376adc5efcabb4bd5697faba6f03425d7c
Andre-Junior <andrejunior.dias@gmail.com>
2021-05-08 18:50:09 -0300

    rederizacao
    Renderização condicional, exemplo dado na documentação do react

---
+++ rederizacao-condicional/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import LoginControl from './components/loginControl'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LoginControl />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
